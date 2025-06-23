ff85bcf1106981221e8e811a83dc6360cbd0a1cf
BrandonDev1996 <brandondev1996@gmail.com>
2025-06-23 16:41:26 -0300

    feat: add btn close in sidenav

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -4,8 +4,10 @@ fetch('/views/sidenav.html')
     .then(res => res.text())
     .then(html => {
         sideNavContainer.innerHTML = html;
+
         const sideNavElement = document.getElementById('sideNav');
         const btnToggleCart = document.getElementById('btnToggleCart');
+        const btnCloseSideNav = document.getElementById('btnCloseSideNav'); // ahora sí existe
 
         btnToggleCart.addEventListener('click', () => {
             if (sideNavElement.style.right === '0px') {
@@ -13,6 +15,10 @@ fetch('/views/sidenav.html')
             } else {
                 sideNavElement.style.right = '0px';
             }
+        });
+
+        btnCloseSideNav.addEventListener('click', () => {
+            sideNavElement.style.right = '-300px';
         });
     })
     .catch(err => console.error('Error cargando el sidenav:', err));
